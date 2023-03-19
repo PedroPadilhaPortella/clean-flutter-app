700b4dc49bded5d5cc8abe0310c49e25fffcc201
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-03-19 02:02:20 -0300

    feat: add HttpClient and RemoteAuthentication

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,44 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({@required String url, @required String method}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    await client.post(Uri.dataFromString(url), headers: headers);
+  }
+}
+
+class ClientSpy extends Mock implements Client {}
+
+void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+
+  group('post', () {
+    test('Should call post with correct values', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(Uri.dataFromString(url), headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
+    });
+  });
+}
